342d0b44dee2468319984dfdeedbd4131f1ae4da
Alfaridzi <irvanalfaridzi@gmail.com>
2020-08-05 13:51:38 +0700

    belajar if

--- bin/my_first_dart_console.dart
+++ bin/my_first_dart_console.dart
@@ -1,11 +1,24 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-//  inputan
-  String a = stdin.readLineSync();
-//  parse integer
-  int b = int.tryParse(a);
-//  parse double
-  double c = double.tryParse(a);
-  print(c + 10);
+////  belajar inputan
+//  String a = stdin.readLineSync();
+////  parse integer
+//  int b = int.tryParse(a);
+////  parse double
+//  double c = double.tryParse(a);
+//  print(c + 10);
+
+//  Belajar if
+  int input = int.tryParse(stdin.readLineSync());
+  if(input > 0){
+    print("positif");
+  } else if(input<0){
+    print("negatif");
+  } else{
+    print("nol");
+  }
+
+
 }
+
